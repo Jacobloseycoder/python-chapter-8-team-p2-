283dfe10ad08c901408ec04308ceb23fa253c20d
jacob losey <loseyjacob1@gmail.com>
2025-02-17 13:03:11 -0600

    Update and rename main to main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+def usinput():
+#ask user for a string
+#check if the string is at least 5 letters and only leters
+def reverse():
+#revers the string and return it
+def counter():
+#counts the number of vawles and constints
+def replace():
+#replace spicific letters with another letter
+def menu():
+#let user deside what function to do
+#loop till exit is selected
+def analysis():
+#retuns if string is a palindrone
+#retuns total letters
+#returns total constins (call counter to make it faster)
+#returns total vawls (call counter to make it faster)
