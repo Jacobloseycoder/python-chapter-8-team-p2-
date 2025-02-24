abfab56a3040aad3a81cd1162b620668f8c12164
jacob losey <loseyjacob1@gmail.com>
2025-02-24 13:08:20 -0600

    Update main.py
    Signed-off-by: jacob losey <loseyjacob1@gmail.com>

--- main.py
+++ main.py
@@ -3,10 +3,27 @@ def usinput():
 #check if the string is at least 5 letters and only leters
 def reverse():
 #revers the string and return it
-def counter():
+def counter(string):
 #counts the number of vawles and constints
-def replace():
+  constints = 0
+  vawles = 0
+  bill = 0
+  for letter in string:
+    timmy = string[bill]
+    bill = bill + 1
+    if timmy = 'a' or timmy = 'e' or timmy = 'i' or timmy = 'o' or timmy = 'u':
+      vawles = vawles + 1
+    elif timmy = 'A' or timmy = 'E' or timmy = 'I' or timmy = 'O' or timmy = 'U':
+      vawles = vawles + 1
+    else:
+      constints = constints + 1
+return vawles, constints
+def replace(string):
 #replace spicific letters with another letter
+  timmy = input('enter the letter you want to replace')
+  bimmy = input('enter the letter to replace it with')
+  string.replase(timmy, bimmy)
+  return string
 def menu():
 #let user deside what function to do
 #loop till exit is selected
