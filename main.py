f338882b266a44504dd8438821c84b085d63c3e9
Evernaught14 <ebrown5178@gmail.com>
2025-03-04 13:55:25 -0600

    Update main.py
    Signed-off-by: Evernaught14 <ebrown5178@gmail.com>

--- main.py
+++ main.py
@@ -44,37 +44,42 @@ def menu():
     choice = 0
     
     
-def usinput():#program 1
+def usinput():
+    #program 1
     #ask the user for a string
     #check if the strig is at least five letters and only letters
+    global user_input
     count = 0
-    user_input = input(f"Enter a rale word that is five letters and only letters")
+    user_input = input(f"Enter a rale word that is five letters and only letters: ")
     if user_input.isalpha():
         for letters in user_input:
-            count = count + 1
+            count = (count + 1)
         if count > 4:
-            return user_input
+            m = 0
     else:
         print('the user input is not exsepted')
-    
+    menu()
 
 def reverse():
+    w = 0
 #revers the string and return it
-def counter(string):
-#counts the number of vawles and constints
-  constints = 0
-  vawles = 0
-  bill = 0
-  for letter in user_input:
-    timmy = user_input[bill]
-    bill = bill + 1
-    if timmy == 'a' or timmy == 'e' or timmy == 'i' or timmy == 'o' or timmy == 'u':
-      vawles = vawles + 1
-    elif timmy == 'A' or timmy == 'E' or timmy == 'I' or timmy == 'O' or timmy == 'U':
-      vawles = vawles + 1
-    else:
-      constints = constints + 1
-return vawles, constints
+    moo = 1
+def counter():
+    global user_input
+    constints = 0
+    vawles = 0
+    bill = 0
+    for letter in user_input:
+        timmy = user_input[bill]
+        bill = (bill + 1)
+        if timmy == 'a' or timmy == 'e' or timmy == 'i' or timmy == 'o' or timmy == 'u':
+          vawles = vawles + 1
+        elif timmy == 'A' or timmy == 'E' or timmy == 'I' or timmy == 'O' or timmy == 'U':
+          vawles = vawles + 1
+        else:
+          constints = constints + 1
+    return vawles, constints
+
 def replace(user_input):
 #replace spicific letters with another letter
   timmy = input('enter the letter you want to replace')
@@ -83,6 +88,16 @@ def replace(user_input):
   return user_input
 
 def analysis():
+    letter = 0
+    1 == 'm'
+    for letters in user_input:
+        letter = (letter + 1)
+        letternow = letters
+        letterw = letter
+        letterw = letters
+        print(letter)
+        print(letterw)
+        
 #retuns if string is a palindrone
 #retuns total letters
 #returns total constins (call counter to make it faster)
